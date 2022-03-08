f7afa5e451b41e2d8173b170e058cb7cf837494d
TheInfinitus <sabacreativo@gmail.com>
2022-03-08 20:49:56 +0000

    Javascript: City Search
    Added all Javascript for the Search Bar to work. Tested locally and working

--- src/app.js
+++ src/app.js
@@ -32,3 +32,21 @@ let minutes = now.getMinutes();
 
 let realDate = document.querySelector("#hourInfo");
 realDate.innerHTML = `${hour}:${minutes} - ${dayName}, ${date} ${monthName}, ${yearnumber}`;
+
+let realCity = document.querySelector("#city-form");
+realCity.addEventListener("submit", CitySearch);
+
+function CitySearch(event) {
+  event.preventDefault();
+  let city = document.querySelector(".main-city");
+  let cityResult = document.querySelector("#search-engine");
+  city.innerHTML = cityResult.value;
+  let apiKey = "4ef94b768e7d2f21afad485d2138b2d7";
+  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityResult.value}&appid=${apiKey}&units=metric`;
+  axios.get(apiURL).then(DefaultTemperature);
+}
+function DefaultTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let heading = document.querySelector("h1");
+  heading.innerHTML = `${temperature}°C`;
+}
